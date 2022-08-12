019af7b11e1767ea539bb6a64ff1a8dc9b89abb3
Cardoso <cardosoassuncao@gmail.com>
2022-08-12 10:12:53 -0400

    add transaction table

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 64px auto 0;
+  padding: 0 24px;
+`;
 
-`
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 8px;
+  /* margin-top: 24px; */
+  td {
+    padding: 20px 32px;
+    background: ${props => props.theme["gray-700"]};
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${props => props.variant === "income" ? props.theme["green-300"] : props.theme["red-300"]};
+`;
